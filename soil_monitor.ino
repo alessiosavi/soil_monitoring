b6934b8c0d7505d0f9cab3f4560a85dcc1d0ae8b
alessiosavi <alessiosavibtc@gmail.com>
2020-12-23 21:56:31 +0100

    `soil_monitor.ino`:      - Develop custom method for retrieve analog data (`analogSoil()`, `analogLight()`)      - Develop method for flash the light n time (`flash_n_times`) in order to be sure that data are sent correctly

--- soil_monitor.ino
+++ soil_monitor.ino
@@ -1,23 +1,54 @@
 #include <DHT.h>
 #include <ESP8266HTTPClient.h>
-#include <ESP8266WiFi.h> // Include the Wi-Fi library
+#include <ESP8266WiFi.h>
 #include <time.h>
 
-#define DHTPIN1 5
-#define DHTPIN2 12
-#define SOIL_PIN A0
-#define LIGHT_PIN 10
+#define DHTPIN1 12  // D6
+#define DHTPIN2 14  // D5
+#define SOIL_PIN 4  // D2
+#define LIGHT_PIN 5 // D1
+
 const char *ssid = "TIM-29854979";
 const char *password = "billgatesfinocchio";
 
 DHT dht[] = {
-  {DHTPIN1, DHT11},
-  //    {DHTPIN2, DHT11},
+    {DHTPIN1, DHT11},
+    {DHTPIN2, DHT11},
 };
-
-// DHT dht1(DHTPIN1, DHTTYPE);
-// DHT dht2(DHTPIN2, DHTTYPE);
 WiFiServer server(80);
+
+int analogSoil() {
+  delay(100);
+  digitalWrite(SOIL_PIN, HIGH);
+  delay(100);
+  digitalWrite(LIGHT_PIN, LOW);
+  delay(100);
+  return analogRead(0);
+}
+
+int analogLight() {
+  delay(100);
+  digitalWrite(LIGHT_PIN, HIGH);
+  delay(100);
+  digitalWrite(SOIL_PIN, LOW);
+  delay(100);
+  return analogRead(0);
+}
+
+void led_off() { digitalWrite(LED_BUILTIN, HIGH); }
+void led_on() { digitalWrite(LED_BUILTIN, LOW); }
+
+// flash_n_times is delegated to produce N flash from the standard LED of the
+// eps8266
+void flash_n_times(int n) {
+  led_off();
+  for (int i = 0; i < n; i++) {
+    led_on();
+    delay(50);
+    led_off();
+    delay(200);
+  }
+}
 
 void connect() {
   WiFi.begin(ssid, password); // Connect to the network
@@ -54,7 +85,8 @@ void setup() {
   pinMode(SOIL_PIN, INPUT);
 }
 
-HTTPClient http; // Declare an object of class HTTPClient
+// Declare an object of class HTTPClient
+HTTPClient http;
 
 void loop() {
   if (WiFi.status() != WL_CONNECTED) {
@@ -63,20 +95,27 @@ void loop() {
   }
   float temp = -1;
   float hum = -1;
-  int light = -1;
-  int soil = -1;
+  //  int light = -1;
+  //  int soil = -1;
   for (auto &sensor : dht) {
+
+    // Read and send temperature
     temp = sensor.readTemperature(false, true);
     send_data(temp, "temp");
+
+    // Read and send the humidity
     hum = sensor.readHumidity(false);
     send_data(hum, "hum");
-    soil = analogRead(SOIL_PIN);
-    Serial.println(soil);
-    send_data(soil , "soil");
 
-    light = analogRead(LIGHT_PIN); // read the state of the LDR value
-    Serial.println(light);
-    send_data(light , "light");
+    //    // Read and send light
+    //    light = analogLight();
+    //    Serial.println(light);
+    //    send_data(light , "light");
+    //
+    //    // Read and send soil
+    //    soil = analogSoil();
+    //    Serial.println(soil);
+    //    send_data(soil , "soil");
   }
   delay(2000);
 }
@@ -88,6 +127,7 @@ void send_data(float data, String type) {
     String payload = http.getString(); // Get the request response payload
     Serial.println("REQUEST NOT SUCCESFULLY!");
     Serial.println(payload); // Print the response payload
+    flash_n_times(2);
   }
   http.end(); // Close connection
 }
